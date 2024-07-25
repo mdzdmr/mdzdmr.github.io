d9501e16d63320df8f7a415b5db4f973072e6a8b
Mohammed Zaid Mir <mzaidmir111@gmail.com>
2024-07-24 22:40:28 -0400

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",  // <=== enables static exports
-  reactStrictMode: true,
+    output: 'export',
+    images: {
+        unoptimized: true,
+    },
+    basePath: '/mdzdmr.github.io',  // Replace with your repository name
+    assetPrefix: '/mdzdmr.github.io',  // Replace with your repository name
 };
 
-module.exports = nextConfig;
+export default nextConfig;
